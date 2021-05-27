b287f0491bec2e0ce29d59ba67d6aabb2448427f
Rafael Zanella <rafzane@gmail.com>
2021-05-27 19:57:50 +0100

    push now creates a PR

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { GitlabServiceConfig } from "./types/plugin.type";
 
 const gitlabSyncConfigKey = 'gitlab-sync:config'
 const gitlabSyncLastCommitKey = 'gitlab-sync:lastCommit'
-const defaultConfig = '{"api_url": "", "token": "", "id_project": "", "files": [], "ref": ""}'
+const defaultConfig = '{"api_url": "", "token": "", "id_project": 0, "files": ["workspace.json"], "branch": "master"}'
 
 async function fetchOrSetConfig(context: InsomniaContext): Promise<GitlabServiceConfig> {
   try {
@@ -79,7 +79,7 @@ module.exports.workspaceActions = [
     },
   },
   {
-    label: 'GitLab - Pull Collection',
+    label: 'GitLab - Pull Collections',
     icon: 'fa-download',
     action: async (context: InsomniaContext, models: InsomniaWorkspaceActionModel) => {
       try {
@@ -98,23 +98,19 @@ module.exports.workspaceActions = [
     },
   },
   {
-    label: 'GitLab - Push Collection',
+    label: 'GitLab - Push Collections',
     icon: 'fa-upload',
     action: async (context: InsomniaContext, models: InsomniaWorkspaceActionModel) => {
       try {
-        const provider = await loadProvider(context)
-
-        const data = await context.data.export.insomnia({
-          includePrivate: false,
-          format: 'json',
-          // workspace: models.workspace
-        });
+        const data = await context.data.export.insomnia({ includePrivate: false, format: 'json' });
 
         const content = JSON.stringify(JSON.parse(data), null, 2)
 
-        await provider.update(content)
+        const provider = await loadProvider(context)
 
-        await context.app.alert('GitLab - Push Collection', 'Process concluded')
+        const mergeRequestURL = await provider.update(content)
+
+        await context.app.alert('GitLab - Push Collection', `See your PR: ${mergeRequestURL}`)
       } catch (e) {
         await context.app.alert('Collection update error for the project', e.message)
       }
